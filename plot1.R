fdf10671c76c75b90950d6031cf52c9e251d33cc
bikegeek <minna_win@fastmail.fm>
2015-05-09 18:27:16 -0600

    using Feb1 and Feb2 data only

--- plot1.R
+++ plot1.R
@@ -1,15 +1,15 @@
 plot1 <- function(){
-      #Read in the February data
-      df <- read.table("/Users/Coursera/feb_household_power_consumption.txt", sep=";",header=TRUE,stringsAsFactors=FALSE)
+    #Read in the February data
+    df <- read.table("/Users/MiniMe/Coursera/ExploratoryDataAnalysis/Project1/feb_household_power_consumption.txt", sep=";",header=TRUE,stringsAsFactors=FALSE)
 
-      #Only use the rows without missing data, which is indicated with '?', rather
-      #than NA. Convert the Global active power data to character first.
-      df$Global<- as.character(df$Global_active_power)
-      subdf <- subset(df, df$Global_active_power != '?')
-      gapower <- (df$Global_active_power)
-      png(filename="plot1.png", width=480, height=480, bg="white")
-      hist(as.numeric(gapower),xlab="Global Active Power (kilowatts)", ylab="Frequency",main="Global Active Power",col="red")
+    #Only use the rows without missing data, which is indicated with '?', rather
+    #than NA. Convert the Global active power data to character first.
+    df$Global<- as.character(df$Global_active_power)
+    subdf <- subset(df, df$Global_active_power != '?')
+    gapower <- (df$Global_active_power)
+    png(filename="plot1.png", width=480, height=480, bg="white")
+    hist(as.numeric(gapower),xlab="Global Active Power (kilowatts)", ylab="Frequency",main="Global Active Power",col="lightblue")
 
-      #IMPORTANT, close the device!
-      dev.off()
+    #IMPORTANT, close the device!
+    dev.off()
 }
